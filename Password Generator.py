ce7c925855538feae2e8e7e1263727f95e9a5663
MuhammadOwaisIqbal19 <128293194+MuhammadOwaisIqbal19@users.noreply.github.com>
2023-11-04 12:12:22 +0500

    Add files via upload
    TASK2(RANDOM PASSWORD GENERATOR)

---
+++ Password Generator.py
@@ -0,0 +1,14 @@
+
+#--------------------------------------------SIMPLE CALCULATOR---------------------------------------
+#IMPORTING LIBRARIES
+import string
+import random
+
+#TAKING INPUT FROM THE USER
+PasswordLen=int(input('How much Characters your Password Should contain?\n: '))
+password=''
+for i in range(PasswordLen):
+    password+=random.choice(string.printable)
+
+
+print('Your Password generated is :',password)
